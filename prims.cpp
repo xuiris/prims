cbe292791aafc01b8d6e25149f477bf6b9387619
Iris Xu <axlen@Iriss-MacBook-Pro.local>
2017-12-18 18:28:48 -0800

    Rearranged prims algorithm. Put heapify and placing min into the heap at the end since our "node 1" is a dummy node and has no real cost.

--- prims.cpp
+++ prims.cpp
@@ -111,11 +111,6 @@ void MST::printmst() {
 MST prims(Minheap hp, Graph gr) {
     MST tree;
     while (hp.isempty() == false) {
-        hp.heapify();
-        // put min of heap into tree
-        pair<int,int> minedge = gr.edges[hp.h[0].first];
-        tree.mst_edges.push_back(minedge);
-        int min = hp.minnode();
         // delete min
         hp.delmin();
         // update the key in the heap for nodes adjacent to deleted min
@@ -129,6 +124,11 @@ MST prims(Minheap hp, Graph gr) {
                 hp.h[hp.index[node]].first = cost;
             }
         }
+        hp.heapify();
+        // put min of heap into tree
+        pair<int,int> minedge = gr.edges[hp.h[0].first];
+        tree.mst_edges.push_back(minedge);
+        int min = hp.minnode();
     }
     return tree;
 }
